08fd2735302a2991f153b712763d89db6cf80b6c
Gregory Holt <gregory.holt@gmail.com>
2015-03-02 23:07:00 +0000

    ring_test.go

---
+++ builder_test.go
@@ -0,0 +1,88 @@
+package ring
+
+import (
+	"fmt"
+	"os"
+	"runtime/pprof"
+	"testing"
+	"time"
+)
+
+const RUN_LONG = false
+
+type TestNode struct {
+	nodeID     uint64
+	active     bool
+	capacity   uint32
+	tierValues []int
+}
+
+func (node *TestNode) NodeID() uint64 {
+	return node.nodeID
+}
+
+func (node *TestNode) Active() bool {
+	return node.active
+}
+
+func (node *TestNode) Capacity() uint32 {
+	return node.capacity
+}
+
+func (node *TestNode) TierValues() []int {
+	return node.tierValues
+}
+
+func TestNewRingBuilder(t *testing.T) {
+	if !RUN_LONG {
+		return
+	}
+	f, err := os.Create("ring_test.prof")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	fmt.Println(" nodes inactive partitions bits capacity maxunder maxover seconds")
+	for zones := 10; zones <= 200; {
+		helperTestNewRingBuilder(t, zones)
+		if zones < 100 {
+			zones += 10
+		} else {
+			zones += 100
+		}
+	}
+	pprof.StopCPUProfile()
+}
+
+func helperTestNewRingBuilder(t *testing.T, zones int) {
+	builder := NewBuilder(3)
+	nodeID := uint64(0)
+	//capacity := uint32(1)
+	capacity := uint32(100)
+	for zone := 0; zone < zones; zone++ {
+		for server := 0; server < 50; server++ {
+			for device := 0; device < 2; device++ {
+				nodeID++
+				builder.Add(&TestNode{nodeID: nodeID, active: true, capacity: capacity, tierValues: []int{server, zone}})
+				//capacity++
+				//if capacity > 100 {
+				//	capacity = 1
+				//}
+			}
+		}
+	}
+	start := time.Now()
+	builder.Ring(0)
+	stats := builder.Stats()
+	fmt.Printf("%6d %8d %10d %4d %8d %7.02f%% %6.02f%% %7d\n", stats.NodeCount, stats.InactiveNodeCount, stats.PartitionCount, stats.PartitionBitCount, stats.TotalCapacity, stats.MaxUnderNodePercentage, stats.MaxOverNodePercentage, int(time.Now().Sub(start)/time.Second))
+	builder.Node(25).(*TestNode).active = false
+	start = time.Now()
+	builder.Ring(0)
+	stats = builder.Stats()
+	fmt.Printf("%6d %8d %10d %4d %8d %7.02f%% %6.02f%% %7d\n", stats.NodeCount, stats.InactiveNodeCount, stats.PartitionCount, stats.PartitionBitCount, stats.TotalCapacity, stats.MaxUnderNodePercentage, stats.MaxOverNodePercentage, int(time.Now().Sub(start)/time.Second))
+	builder.Node(20).(*TestNode).capacity = 75
+	start = time.Now()
+	builder.Ring(0)
+	stats = builder.Stats()
+	fmt.Printf("%6d %8d %10d %4d %8d %7.02f%% %6.02f%% %7d\n", stats.NodeCount, stats.InactiveNodeCount, stats.PartitionCount, stats.PartitionBitCount, stats.TotalCapacity, stats.MaxUnderNodePercentage, stats.MaxOverNodePercentage, int(time.Now().Sub(start)/time.Second))
+}
--- ring_test.go
+++ ring_test.go
@@ -1,88 +1,73 @@
 package ring
 
-import (
-	"fmt"
-	"os"
-	"runtime/pprof"
-	"testing"
-	"time"
-)
+import "testing"
 
-const RUN_LONG = false
-
-type TestNode struct {
-	nodeID     uint64
-	active     bool
-	capacity   uint32
-	tierValues []int
-}
-
-func (node *TestNode) NodeID() uint64 {
-	return node.nodeID
-}
-
-func (node *TestNode) Active() bool {
-	return node.active
-}
-
-func (node *TestNode) Capacity() uint32 {
-	return node.capacity
-}
-
-func (node *TestNode) TierValues() []int {
-	return node.tierValues
-}
-
-func TestNewRingBuilder(t *testing.T) {
-	if !RUN_LONG {
-		return
+func TestRingVersion(t *testing.T) {
+	v := (&ringImpl{version: 1}).Version()
+	if v != 1 {
+		t.Fatalf("Version() gave %d instead of 1", v)
 	}
-	f, err := os.Create("ring_test.prof")
-	if err != nil {
-		t.Fatal(err)
-	}
-	pprof.StartCPUProfile(f)
-	fmt.Println(" nodes inactive partitions bits capacity maxunder maxover seconds")
-	for zones := 10; zones <= 200; {
-		helperTestNewRingBuilder(t, zones)
-		if zones < 100 {
-			zones += 10
-		} else {
-			zones += 100
-		}
-	}
-	pprof.StopCPUProfile()
 }
 
-func helperTestNewRingBuilder(t *testing.T, zones int) {
-	builder := NewBuilder(3)
-	nodeID := uint64(0)
-	//capacity := uint32(1)
-	capacity := uint32(100)
-	for zone := 0; zone < zones; zone++ {
-		for server := 0; server < 50; server++ {
-			for device := 0; device < 2; device++ {
-				nodeID++
-				builder.Add(&TestNode{nodeID: nodeID, active: true, capacity: capacity, tierValues: []int{server, zone}})
-				//capacity++
-				//if capacity > 100 {
-				//	capacity = 1
-				//}
-			}
-		}
+func TestRingPartitionBitCount(t *testing.T) {
+	v := (&ringImpl{partitionBitCount: 1}).PartitionBitCount()
+	if v != 1 {
+		t.Fatalf("PartitionBitCount() gave %d instead of 1", v)
 	}
-	start := time.Now()
-	builder.Ring(0)
-	stats := builder.Stats()
-	fmt.Printf("%6d %8d %10d %4d %8d %7.02f%% %6.02f%% %7d\n", stats.NodeCount, stats.InactiveNodeCount, stats.PartitionCount, stats.PartitionBitCount, stats.TotalCapacity, stats.MaxUnderNodePercentage, stats.MaxOverNodePercentage, int(time.Now().Sub(start)/time.Second))
-	builder.Node(25).(*TestNode).active = false
-	start = time.Now()
-	builder.Ring(0)
-	stats = builder.Stats()
-	fmt.Printf("%6d %8d %10d %4d %8d %7.02f%% %6.02f%% %7d\n", stats.NodeCount, stats.InactiveNodeCount, stats.PartitionCount, stats.PartitionBitCount, stats.TotalCapacity, stats.MaxUnderNodePercentage, stats.MaxOverNodePercentage, int(time.Now().Sub(start)/time.Second))
-	builder.Node(20).(*TestNode).capacity = 75
-	start = time.Now()
-	builder.Ring(0)
-	stats = builder.Stats()
-	fmt.Printf("%6d %8d %10d %4d %8d %7.02f%% %6.02f%% %7d\n", stats.NodeCount, stats.InactiveNodeCount, stats.PartitionCount, stats.PartitionBitCount, stats.TotalCapacity, stats.MaxUnderNodePercentage, stats.MaxOverNodePercentage, int(time.Now().Sub(start)/time.Second))
+}
+
+func TestRingReplicaCount(t *testing.T) {
+	v := (&ringImpl{replicaToPartitionToNodeIndex: make([][]int32, 3)}).ReplicaCount()
+	if v != 3 {
+		t.Fatalf("ReplicaCount() gave %d instead of 3", v)
+	}
+}
+
+func TestRingLocalNodeID(t *testing.T) {
+	v := (&ringImpl{localNodeIndex: -1}).LocalNodeID()
+	if v != 0 {
+		t.Fatalf("LocalNodeID() gave %d instead of 0", v)
+	}
+	v = (&ringImpl{localNodeIndex: 0, nodeIDs: []uint64{123, 456, 789}}).LocalNodeID()
+	if v != 123 {
+		t.Fatalf("LocalNodeID() gave %d instead of 0", v)
+	}
+	v = (&ringImpl{localNodeIndex: 1, nodeIDs: []uint64{123, 456, 789}}).LocalNodeID()
+	if v != 456 {
+		t.Fatalf("LocalNodeID() gave %d instead of 0", v)
+	}
+}
+
+func TestRingResponsible(t *testing.T) {
+	v := (&ringImpl{localNodeIndex: -1}).Responsible(123)
+	if v {
+		t.Fatal("Responsible(123) gave true instead of false")
+	}
+	d := make([][]int32, 3)
+	d[0] = []int32{0, 1, 2}
+	d[1] = []int32{3, 4, 5}
+	d[2] = []int32{6, 7, 8}
+	v = (&ringImpl{localNodeIndex: 0, replicaToPartitionToNodeIndex: d}).Responsible(0)
+	if !v {
+		t.Fatal("Responsible(0) gave false instead of true")
+	}
+	v = (&ringImpl{localNodeIndex: 0, replicaToPartitionToNodeIndex: d}).Responsible(1)
+	if v {
+		t.Fatal("Responsible(1) gave true instead of false")
+	}
+}
+
+func TestRingResponsibleIDs(t *testing.T) {
+	d := make([][]int32, 3)
+	d[0] = []int32{0, 1, 2}
+	d[1] = []int32{3, 4, 5}
+	d[2] = []int32{6, 7, 8}
+	v := (&ringImpl{nodeIDs: []uint64{10, 11, 12, 13, 14, 15, 16, 17, 18}, replicaToPartitionToNodeIndex: d}).ResponsibleIDs(0)
+	if len(v) != 3 || v[0] != 10 || v[1] != 13 || v[2] != 16 {
+		t.Fatalf("ResponsibleIDs(0) gave %v instead of [10 13 16]", v)
+	}
+	v = (&ringImpl{nodeIDs: []uint64{10, 11, 12, 13, 14, 15, 16, 17, 18}, replicaToPartitionToNodeIndex: d}).ResponsibleIDs(2)
+	if len(v) != 3 || v[0] != 12 || v[1] != 15 || v[2] != 18 {
+		t.Fatalf("ResponsibleIDs(2) gave %v instead of [12 15 18]", v)
+	}
 }
